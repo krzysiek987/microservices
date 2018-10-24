7bcbb516c6092887a62e343cf38ade3d47e31266
k.dorzak@ceplus.com <k.dorzak@ceplus.com>
2018-10-24 23:38:32 +0200

    step 5 - set up mongo, lombok, add Product entity

---
+++ product-service/src/main/java/com/example/microservices/product/Product.java
@@ -0,0 +1,17 @@
+package com.example.microservices.product;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.UUID;
+
+@Data
+@Document
+public class Product {
+
+    @Id
+    private UUID id;
+    private String name;
+
+}
