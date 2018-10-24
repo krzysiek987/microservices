4afedde8161ccf72d55943cfdc7552d8448f0f61
k.dorzak@ceplus.com <k.dorzak@ceplus.com>
2018-10-24 23:38:55 +0200

    step 5 - add repository and controller

---
+++ product-service/src/main/java/com/example/microservices/product/ProductController.java
@@ -0,0 +1,49 @@
+package com.example.microservices.product;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("products")
+public class ProductController {
+
+    private final ProductRepository repository;
+
+    @GetMapping
+    public Iterable<Product> getList(){
+        return repository.findAll();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Product> get(@PathVariable UUID id){
+        return repository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product insert(@RequestBody Product product){
+        return repository.insert(product);
+    }
+
+    @PostMapping("{id}")
+    public Product update(@PathVariable UUID id, @RequestBody Product product){
+        if(!Objects.equals(id, product.getId())) {
+            throw new IllegalArgumentException("Id mismatch");
+        }
+        return repository.save(product);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id){
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ product-service/src/main/java/com/example/microservices/product/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.example.microservices.product;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, UUID> {
+
+
+}
