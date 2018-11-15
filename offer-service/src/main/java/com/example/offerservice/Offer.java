5ef3d094d36b0ce27ca787720431d635eb050561
k.dorzak@ceplus.com <k.dorzak@ceplus.com>
2018-11-15 12:13:19 +0100

    entity extended

--- offer-service/src/main/java/com/example/offerservice/Offer.java
+++ offer-service/src/main/java/com/example/offerservice/Offer.java
@@ -1,5 +1,6 @@
 package com.example.offerservice;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
@@ -12,5 +13,8 @@ public class Offer {
 	@Id
 	private UUID id;
 	private String name;
+	private BigDecimal value;
+	private UUID productId;
+	private UUID bidderId;
 
 }
