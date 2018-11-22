1aaa95562d364c971a59a084d23d170c75ad048e
k.dorzak@ceplus.com <k.dorzak@ceplus.com>
2018-11-22 11:11:37 +0100

    backup

---
+++ config-service/src/test/java/com/example/config/DiscoveryServiceApplicationTest.java
@@ -0,0 +1,16 @@
+package com.example.config;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class DiscoveryServiceApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
--- offer-service/src/main/java/com/example/offerservice/OfferServiceApplication.java
+++ offer-service/src/main/java/com/example/offerservice/OfferServiceApplication.java
@@ -2,10 +2,14 @@ package com.example.offerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.util.IdGenerator;
 import org.springframework.util.JdkIdGenerator;
 
+@EnableEurekaClient
+@EnableFeignClients
 @SpringBootApplication
 public class OfferServiceApplication {
 
